9b9be994696db17473f59ff09170e560801f85b6
BhargabLinx <bhargabbhuyan001@gmail.com>
2025-06-19 05:39:07 +0000

    Add button and change card container design

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,13 @@
+import { InputBox } from "./components/InputBox";
+
 function App() {
     return (
         <div className="h-screen bg-slate-900 flex justify-center items-center">
-            <div className="bg-slate-500 w-[450px] h-[400px] rounded-2xl shadow-2xl shadow-cyan-500/50"></div>
+            <div className="bg-slate-500 w-[450px] h-[350px] rounded-2xl shadow-2xl shadow-cyan-500/50 flex flex-col">
+                <InputBox />
+                <InputBox />
+                <button className="flex-1">Convert</button>
+            </div>
         </div>
     );
 }
