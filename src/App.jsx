3b40eba44189e01357d22b60693efa29fc859a62
BhargabLinx <bhargabbhuyan001@gmail.com>
2025-06-19 05:45:08 +0000

    Design button in App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -6,7 +6,9 @@ function App() {
             <div className="bg-slate-500 w-[450px] h-[350px] rounded-2xl shadow-2xl shadow-cyan-500/50 flex flex-col">
                 <InputBox />
                 <InputBox />
-                <button className="flex-1">Convert</button>
+                <button className="flex-1 bg-blue-600 mx-4 mb-4 rounded-xl text-white font-bold cursor-pointer hover:bg-blue-700 duration-100">
+                    Convert
+                </button>
             </div>
         </div>
     );
