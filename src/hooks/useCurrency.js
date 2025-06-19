64be4e380ba786e5b05f6df85ae85b6f2e4ee469
BhargabLinx <bhargabbhuyan001@gmail.com>
2025-06-19 06:35:55 +0000

    Improve the custom hook

--- src/hooks/useCurrency.js
+++ src/hooks/useCurrency.js
@@ -1,9 +1,16 @@
-const useCurrency = async (currency) => {
+import { useEffect, useState } from "react";
+
+const useCurrency = (currency) => {
+    const [data, setData] = useState({});
     const URL = `https://cdn.jsdelivr.net/npm/@fawazahmed0/currency-api@latest/v1/currencies/${currency}.json`;
-    let response = await fetch(URL);
-    response = await response.json();
-    response = response[currency];
-    return response;
+    useEffect(() => {
+        fetch(URL)
+            .then((res) => res.json())
+            .then((res) => setData(res[currency]));
+    }, [currency]);
+    console.log(data);
+
+    return data;
 };
 
 export default useCurrency;
