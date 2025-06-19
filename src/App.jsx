23337f45a8d1c0e710ea30966f5434e989beea0f
BhargabLinx <bhargabbhuyan001@gmail.com>
2025-06-19 05:54:00 +0000

    Take amount from user

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,18 @@
+import { useState } from "react";
 import { InputBox } from "./components/InputBox";
 
 function App() {
+    const [fromAmount, setFromAmount] = useState(0);
+
     return (
         <div className="h-screen bg-slate-900 flex justify-center items-center">
             <div className="bg-slate-500 w-[450px] h-[350px] rounded-2xl shadow-2xl shadow-cyan-500/50 flex flex-col">
-                <InputBox />
-                <InputBox />
+                <InputBox
+                    amountType="From"
+                    amount={fromAmount}
+                    setAmount={setFromAmount}
+                />
+                <InputBox amountType="To" />
                 <button className="flex-1 bg-blue-600 mx-4 mb-4 rounded-xl text-white font-bold cursor-pointer hover:bg-blue-700 duration-100">
                     Convert
                 </button>
--- src/components/InputBox.jsx
+++ src/components/InputBox.jsx
@@ -1,15 +1,17 @@
 import React from "react";
 
-export const InputBox = () => {
+export const InputBox = ({ amountType, amount, setAmount }) => {
     return (
         <div className="bg-slate-100 shadow-md flex justify-between gap-4 m-4 p-6 rounded-2xl">
             <div className="flex flex-col gap-2 w-1/2">
                 <label className="text-slate-700 text-sm font-medium">
-                    From
+                    {amountType}
                 </label>
                 <input
                     type="number"
-                    value={0}
+                    placeholder="0"
+                    value={amount === 0 ? "" : amount}
+                    onChange={(e) => setAmount(e.target.value)}
                     className="px-4 py-2 border border-gray-300 rounded-lg focus:outline-none focus:ring-0 bg-white text-slate-800"
                 />
             </div>
